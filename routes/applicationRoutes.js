9ab3b69dc69ac02609f6f53c5e96f3d5ce06f2f4
angemissiba <as.missiba@b2i-solutions.com>
2024-07-30 16:11:29 +0200

    #v1

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -8,39 +8,84 @@ router.post('/register', async (req, res) => {
     try {
         const application = new Application(req.body);
         await application.save();
-        res.status(201).json({ data: application, message: "Candidature envoyée avec succès" });
+        return res.status(201).json({ data: application, message: "Candidature envoyée avec succès" });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 });
+
+
+
+// Accepté
+router.post('/register/:id', async (req, res) => {
+    try {
+        const idApplication =  req.params.id;
+
+        const applicationExist = await Application({_id:idApplication});
+        if(applicationExist){
+            return res.status(410).json({ message:"Candidature non trouvé"});
+        }
+        applicationExist.status = "ACCEPTED" ;
+
+        await applicationExist.save();
+        
+        return res.status(200).json({ data: applicationExist, message: "Candidature Accepté avec succès" });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
+
 
 // Obtenir toutes les candidatures
 router.get('/get_applications', async (req, res) => {
     try {
-        const applications = await Application.find();
-        res.status(200).json({ data: applications, message: "Toutes les candidatures" });
+        const applications = await Application.find()
+        .populate({
+            path: 'projectId',
+            select: 'title description createdAt category skills' // specify the fields you want
+        })
+        .populate({
+            path: 'artisanId',
+            select: 'firstname lastname phone codePostal email' // specify the fields you want
+        });
+
+        return res.status(200).json({ data: applications, message: "Toutes les candidatures" });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 });
 
 // Obtenir les candidatures par projet
 router.get('/get_applications/project/:projectId', async (req, res) => {
     try {
-        const applications = await Application.find({ projectId: req.params.projectId });
-        res.status(200).json({ data: applications, message: "Toutes les candidatures pour ce projet" });
+        const applications = await Application.find({ projectId: req.params.projectId })
+        .populate({
+            path: 'projectId',
+            select: 'title description createdAt category skills' // specify the fields you want
+        })
+        .populate({
+            path: 'artisanId',
+            select: 'firstname lastname phone codePostal email' // specify the fields you want
+        });
+
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
+
 // Obtenir les candidatures d'un artisan
 router.get('/get_applications/artisan/:artisanId', authenticateToken, async (req, res) => {
     try {
-        const applications = await Application.find({ artisanId: req.params.artisanId });
-        res.status(200).json({ data: applications, message: "Les candidatures d'un artisan" });
+        const applications = await Application.find({ artisanId: req.params.artisanId })
+        .populate("projectId._id,projectId.name,projectId.description,projectId.createdAt,projectId.category,projectId.skills",)
+        .populate("artisanId._id,artisanId.firstname,artisanId.lastname,artisanId.phone,artisanId.codePostal",);
+
+
+        return res.status(200).json({ data: applications, message: "Les candidatures d'un artisan" });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 });
 
@@ -51,9 +96,9 @@ router.patch('/edit/:id', authenticateToken, async (req, res) => {
         if (!application) {
             return res.status(404).send({ error: 'Candidature non trouvée' });
         }
-        res.status(200).json({ data: application, message: "Candidature mise à jour" });
+        return res.status(200).json({ data: application, message: "Candidature mise à jour" });
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
     }
 });
 
@@ -64,9 +109,9 @@ router.delete('/deleted/:id', authenticateToken, async (req, res) => {
         if (!application) {
             return res.status(404).send({ error: 'Candidature non trouvée' });
         }
-        res.status(200).json({ message: 'Candidature supprimée' });
+        return res.status(200).json({ message: 'Candidature supprimée' });
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 });
 
