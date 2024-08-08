4d98eb18dc33db58c15e71d3d608ead7bcaeb396
angemissiba <as.missiba@b2i-solutions.com>
2024-08-08 06:10:06 +0200

    #v1

--- utils/dataApi.js
+++ utils/dataApi.js
@@ -6,6 +6,7 @@
 const ApplicationInfo={
     name :"Plateforme artisan ENTREPRISE 4",
     urlwebSite :  "https://artisan-entreprise-4.web.app",
+    logoWebSite:"https://artisan-entreprise-4.web.app/logoApplication.png",
     description:"Application pour les artisan ",
     emailApplication:"aymarbly559@gmail.com",
     passwordEmail:"a g c t x y x c o x s k v a g k",
